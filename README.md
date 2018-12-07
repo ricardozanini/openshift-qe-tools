808c3ebe7c833365286d17f2b6d726976f83a55a
Ricardo Zanini <ricardozanini@gmail.com>
2018-12-07 18:05:17 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# OpenShift QE Tools
+
+_(Working in Progress) - use at your own discretion_
+
+A bunch of scripts and tools to ease monitoring, testing and health checking OpenShift 3.x clusters.
+
+## 1. Router
+
+Inside the `router` path we have scripts to check the health of OpenShift routes.
+
+### test-routes.py
+
+Simple script to check if all your routes are ok. It basically queries the `router` API and pings every route to check its returned HTTP status code. If something isn't right you'll see colorful `WARN` and `ERROR` messages in your console.
+
+#### How to use
+
+Simply call `python3 test-routes.py`. The script will use the default `system:admin` user and will login at your local cluster. For more details about usage use:
+
+```console
+$ python3 test-routes.py -h
+
+usage: test-routes.py [-h] [-u USER] [-p PASSWORD] [--url URL]
+                      [--skip-login [SKIP_LOGIN]] [-k [IGNORE_SELFSIGN]]
+
+check every route within an OpenShift cluster
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -u USER               the cluster user admin to login
+  -p PASSWORD           the cluster admin password
+  --url URL             the cluster URL
+  --skip-login [SKIP_LOGIN]
+                        completely skip oc login (you must be logged in)
+  -k [IGNORE_SELFSIGN]  ignores self-signed certificates when connecting to
+                        routes and OpenShift API
+```
---
+++ router/test-routes.py
@@ -0,0 +1,103 @@
+import subprocess
+import argparse
+import requests
+import urllib3
+
+# sysadmin default login
+SYSTEM_ADMIN = "system:admin"
+
+# colors
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# status codes. red.: https://httpstatuses.com/
+class httpCodes:
+    SC_OK = 200
+    SC_REDIRECTION = 300
+    SC_CLIENT_ERR = 400
+    SC_SERVER_ERR = 500
+
+# ref.: https://urllib3.readthedocs.io/en/latest/advanced-usage.html#ssl-warnings
+urllib3.disable_warnings()
+
+
+
+def init_parser():
+    parser = argparse.ArgumentParser(description='check every route within an OpenShift cluster')
+    parser.add_argument('-u', dest='user', help='the cluster user admin to login', default=SYSTEM_ADMIN)
+    parser.add_argument('-p', dest='password', help='the cluster admin password')
+    parser.add_argument('--url', dest='url', help='the cluster URL', default='https://localhost:8443')
+    parser.add_argument('--skip-login', dest='skip_login', nargs='?', help='completely skip oc login (you must be logged in)', const=True, default=False)
+    parser.add_argument('-k', dest='ignore_selfsign', nargs='?', help='ignores self-signed certificates when connecting to routes and OpenShift API', const=True, default=False)
+    return parser
+
+def login(user, password, url):
+    if user == SYSTEM_ADMIN:
+        subprocess.call(["oc", "login", "-u", user])
+    else:
+        subprocess.call(["oc", "login", url, "-u", user, "-p", password])
+
+def get_all_routes():
+    output = subprocess.check_output(
+        "oc get routes -o jsonpath='{range .items[*]}[{.spec.host}, {.spec.tls.termination}]{end}' --all-namespaces",
+        stderr=subprocess.STDOUT,
+        shell=True).decode("utf-8")
+    routes = {}
+    if not output:
+        return routes
+
+    for item in output.split('['):
+        if not item:
+            continue
+        route = item[0:-1].split(',') 
+        if not route[1].strip():
+            routes[route[0]] = { 
+                'tls' : False,
+                'protocol' : 'http',
+                'url' : 'http://{}'.format(route[0])
+            }
+        else:
+            routes[route[0]] = { 
+                'tls' : True, 
+                'protocol' : 'https',
+                'url' : 'https://{}'.format(route[0])
+            }
+
+    return routes
+
+def check_routes(routes, ignore_selfsign):
+    for route in routes:
+        print("{}[CHECKING] Route {}{}".format(bcolors.BOLD, route, bcolors.ENDC))
+        try:
+            r = requests.get(routes[route]["url"], verify=not ignore_selfsign)
+        except requests.exceptions.RequestException as err:
+            print("{}[ERROR] Impossible to get to route {}. Error: '{}'".format(bcolors.FAIL, route, err))
+            if isinstance(err, requests.exceptions.SSLError):
+                print("Is this cluster using self-signed certificate? Try running the script again with '-k' parameter.{}".format(bcolors.ENDC))
+            continue
+
+        routes[route]["status"] = r.status_code
+        
+        if r.status_code // httpCodes.SC_OK == 1:
+            print("{}[OK] Route {} is ok. HTTP Status Code is {}{}".format(bcolors.OKGREEN, route, r.status_code, bcolors.ENDC))
+        if r.status_code // httpCodes.SC_REDIRECTION == 1:
+            print("{}[OK] Route {} returned a redirect code. HTTP Status Code is {}{}".format(bcolors.OKGREEN, route, r.status_code, bcolors.ENDC))
+        if r.status_code // httpCodes.SC_CLIENT_ERR == 1:
+            print("{}[WARN] Route {} returned a client error code. HTTP Status is {}{}".format(bcolors.WARNING, route, r.status_code, bcolors.ENDC))
+        if r.status_code // httpCodes.SC_SERVER_ERR == 1:
+            print("{}[ERROR] Route {} returned a server error code. HTTP Status is {}{}".format(bcolors.FAIL, route, r.status_code, bcolors.ENDC))
+        
+        # TODO: check certificates
+
+parser = init_parser().parse_args()
+if not parser.skip_login:
+    login(parser.user, parser.password, parser.url)
+
+check_routes(get_all_routes(), parser.ignore_selfsign)
